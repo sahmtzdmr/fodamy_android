7aad305cd088bea0178531c26f2bec4354250b7f
Sadık Ahmet <sadikahmetozdemir@anadolu.edu.tr>
2021-11-04 18:08:56 +0300

    Packages,shape screen,wievpageradapter created

---
+++ app/src/main/java/com/sadikahmetozdemir/sadik_fodamy/ui/intro/FirstWalkThroughFragment.kt
@@ -0,0 +1,35 @@
+package com.sadikahmetozdemir.sadik_fodamy.ui.intro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sadikahmetozdemir.sadik_fodamy.R
+
+
+
+
+class FirstWalkThroughFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view= inflater.inflate(R.layout.fragment_first_walk_through, container, false)
+
+        val fragmentList= arrayListOf<Fragment>()
+
+
+        return view
+    }
+
+
+}
