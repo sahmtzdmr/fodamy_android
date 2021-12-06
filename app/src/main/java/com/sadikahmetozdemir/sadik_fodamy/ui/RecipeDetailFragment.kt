c1d20368e392cd61ec2c5242182280edf81af66d
Sadık Ahmet <sadikahmetozdemir@anadolu.edu.tr>
2021-12-06 20:55:03 +0300

    Recipe Details add

---
+++ app/src/main/java/com/sadikahmetozdemir/sadik_fodamy/ui/RecipeDetailFragment.kt
@@ -0,0 +1,25 @@
+package com.sadikahmetozdemir.sadik_fodamy.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sadikahmetozdemir.sadik_fodamy.R
+
+class RecipeDetailFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recipe_detail, container, false)
+    }
+
+}
