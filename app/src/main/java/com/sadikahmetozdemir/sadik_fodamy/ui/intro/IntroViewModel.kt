7c82440219d5177ce35b9bea28cd7b3ec3a7a120
Sadık Ahmet <sadikahmetozdemir@anadolu.edu.tr>
2022-02-11 12:34:31 +0300

    Fix Layout Values

--- app/src/main/java/com/sadikahmetozdemir/sadik_fodamy/ui/intro/IntroViewModel.kt
+++ app/src/main/java/com/sadikahmetozdemir/sadik_fodamy/ui/intro/IntroViewModel.kt
@@ -2,4 +2,9 @@ package com.sadikahmetozdemir.sadik_fodamy.ui.intro
 
 import com.sadikahmetozdemir.sadik_fodamy.base.BaseViewModel
 
-class IntroViewModel : BaseViewModel()
+class IntroViewModel : BaseViewModel() {
+
+    fun onClose() {
+        navigate(IntroFragmentDirections.toHomeFragment())
+    }
+}
