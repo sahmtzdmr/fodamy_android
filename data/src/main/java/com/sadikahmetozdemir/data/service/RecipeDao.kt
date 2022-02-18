f42ec4f8713033b10a2643d0b75f68747f748853
Sadık Ahmet <sadikahmetozdemir@anadolu.edu.tr>
2022-02-18 15:15:41 +0300

    Create Room Database

--- data/src/main/java/com/sadikahmetozdemir/data/service/RecipeDao.kt
+++ data/src/main/java/com/sadikahmetozdemir/data/service/RecipeDao.kt
@@ -4,18 +4,25 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import com.sadikahmetozdemir.data.shared.local.dto.CommentDatabase
 import com.sadikahmetozdemir.data.shared.local.dto.RecipeDataBase
 
 @Dao
 interface RecipeDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun  insertRecipes(recipes: List<RecipeDataBase>)
+    suspend fun insertRecipes(recipes: List<RecipeDataBase>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertComments(comments: CommentDatabase)
+
+    @Query("select * from comments")
+    suspend fun getAllComments(recipeID: Int): List<CommentDatabase>
 
     @Query("select * from recipes where is_editor_choice =:recipeType")
-    suspend fun getEditorChoiceRecipes(recipeType:Int):List<RecipeDataBase>
+    suspend fun getEditorChoiceRecipes(recipeType: Int): List<RecipeDataBase>
 
     @Query("select * from recipes")
-    suspend fun getAllRecipes(recipeType: Int):List<RecipeDataBase>
+    suspend fun getAllRecipes(recipeType: Int): List<RecipeDataBase>
 
 }
