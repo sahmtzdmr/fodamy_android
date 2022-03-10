7ee80e9282bb94966b50a118332b9323a8a92d76
Sadık Ahmet <sadikahmetozdemir@anadolu.edu.tr>
2022-03-10 13:53:49 +0300

    Add Mock Server

--- data/src/main/java/com/sadikahmetozdemir/data/shared/repositories/MockRepository.kt
+++ data/src/main/java/com/sadikahmetozdemir/data/shared/repositories/MockRepository.kt
@@ -4,13 +4,19 @@ import androidx.paging.PagingData
 import com.sadikahmetozdemir.data.mappers.toDomainModel
 import com.sadikahmetozdemir.data.mappers.toDomaninModel
 import com.sadikahmetozdemir.data.shared.local.converters.fromJson
+import com.sadikahmetozdemir.data.shared.remote.CategoryModel
+import com.sadikahmetozdemir.data.shared.remote.CommentModel
 import com.sadikahmetozdemir.data.shared.remote.EditorChoiceModel
 import com.sadikahmetozdemir.data.shared.remote.LogoutModel
 import com.sadikahmetozdemir.data.shared.remote.RegisterResponseModel
 import com.sadikahmetozdemir.data.shared.utils.AUTH_LOGIN_SUCCESS
 import com.sadikahmetozdemir.data.shared.utils.AUTH_LOGOUT_ERROR
 import com.sadikahmetozdemir.data.shared.utils.AUTH_REGISTER_SUCCESS
+import com.sadikahmetozdemir.data.shared.utils.FAVORITES_RECIPE_SUCCESS
 import com.sadikahmetozdemir.data.shared.utils.FEED_RECIPE_SUCCESS
+import com.sadikahmetozdemir.data.shared.utils.LIKE_REQUEST_SUCCESS
+import com.sadikahmetozdemir.data.shared.utils.RECIPE_COMMENTS_SUCCESS
+import com.sadikahmetozdemir.data.shared.utils.USER_FOLLOW_SUCCESS
 import com.sadikahmetozdemir.data.utils.JsonReader
 import com.sadikahmetozdemir.domain.entities.Auth
 import com.sadikahmetozdemir.domain.entities.BaseModel
@@ -69,41 +75,65 @@ class MockRepository @Inject constructor(private val jsonReader: JsonReader) :
 
     override suspend fun getRecipeDetail(recipeID: Int): Recipe {
         return execute {
-            val jsonData=jsonReader.readJson(FEED_RECIPE_SUCCESS)
+            val jsonData = jsonReader.readJson(FEED_RECIPE_SUCCESS)
             fromJson<EditorChoiceModel>(jsonData).toDomaninModel()
         }
     }
 
     override suspend fun getRecipeDetailComment(recipeID: Int): Comment {
-        TODO("Not yet implemented")
+        return execute {
+            val jsonData = jsonReader.readJson(RECIPE_COMMENTS_SUCCESS)
+            fromJson<CommentModel>(jsonData).toDomainModel()
+        }
     }
 
     override suspend fun favoriteRecipesRequest(page: Int): List<Category> {
-        TODO("Not yet implemented")
+        return listOf(execute {
+            val jsonData = jsonReader.readJson(FAVORITES_RECIPE_SUCCESS)
+            fromJson<CategoryModel>(jsonData).toDomainModel()
+        })
     }
 
     override suspend fun favoriteCategoriesRequest(categoryID: Int, page: Int): List<Recipe> {
-        TODO("Not yet implemented")
+        return listOf(execute {
+            val jsonData = jsonReader.readJson(FAVORITES_RECIPE_SUCCESS)
+            fromJson<EditorChoiceModel>(jsonData).toDomaninModel()
+        })
     }
 
     override suspend fun userRecipeLikeRequest(recipeID: Int): BaseModel {
-        TODO("Not yet implemented")
+        return execute {
+            val jsonData = jsonReader.readJson(LIKE_REQUEST_SUCCESS)
+            fromJson<com.sadikahmetozdemir.data.shared.remote.BaseModel>(jsonData).toDomainModel()
+        }
     }
 
     override suspend fun userRecipeDislikeRequest(recipeID: Int): BaseModel {
-        TODO("Not yet implemented")
+        return execute {
+            val jsonData = jsonReader.readJson(LIKE_REQUEST_SUCCESS)
+            fromJson<com.sadikahmetozdemir.data.shared.remote.BaseModel>(jsonData).toDomainModel()
+        }
     }
 
     override suspend fun userFollowRequest(followedID: Int): BaseModel {
-        TODO("Not yet implemented")
+        return execute {
+            val jsonData = jsonReader.readJson(USER_FOLLOW_SUCCESS)
+            fromJson<com.sadikahmetozdemir.data.shared.remote.BaseModel>(jsonData).toDomainModel()
+        }
     }
 
     override suspend fun userUnfollowRequest(followedID: Int): BaseModel {
-        TODO("Not yet implemented")
+        return execute {
+            val jsonData = jsonReader.readJson(USER_FOLLOW_SUCCESS)
+            fromJson<com.sadikahmetozdemir.data.shared.remote.BaseModel>(jsonData).toDomainModel()
+        }
     }
 
     override suspend fun recipeCommentsRequest(categoryID: Int, page: Int): List<Comment> {
-        TODO("Not yet implemented")
+        return listOf(execute {
+            val jsonData = jsonReader.readJson(RECIPE_COMMENTS_SUCCESS)
+            fromJson<CommentModel>(jsonData).toDomainModel()
+        })
     }
 
     override suspend fun postRecipeCommentRequest(recipeID: Int, text: String): Comment {
